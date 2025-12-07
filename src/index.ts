c1095dd53c066e9e2d4746949319424958642e19
BoweJordyn-FS <jkbowe@student.fullsail.edu>
2025-12-07 12:29:36 -0500

    Card CSS and flip logic

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
 // Imports your SCSS stylesheet
 import '@/styles/index.scss';
+const cards = document.querySelectorAll('.card');
+
+let hasFlippedCard: boolean = false;
+let firstCard;
+let secondCard;
+
+const flipCard = (event: Event) => {
+  const card = event.currentTarget as HTMLElement;
+  card.classList.add('flipped');
+    if (!hasFlippedCard) {  
+    hasFlippedCard = true;
+    firstCard = card;
+    return;
+  } else {
+    secondCard = card;
+    hasFlippedCard = false;
+  }
+};
+
+cards.forEach((card) => {
+  card.addEventListener('click', flipCard);
+});
